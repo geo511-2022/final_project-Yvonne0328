7f2d961713bc9d7a94093011051d27ac4668aae9
Yvonne Huang <yonchien@buffalo.edu>
2022-11-05 06:59:17 -0400

    20221105 0659

--- project_test.R
+++ project_test.R
@@ -1,13 +1,20 @@
 library(twitteR) 
 library(rtweet)
 library(dplyr)
+library(tidytext)
+library(textdata)
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(ggplot2)
+library(reshape2)
 # Change consumer_key, consume_secret, access_token, and 
 # access_secret based on your own keys
 
 
 # read data
-LA <- read.csv("data/disaster_LA.csv")
-ALL <- read.csv("data/disaster_ALL.csv")
+data <- read.csv("data/Twitter_disaster_1025_1103.csv")
 
 # word clouds
 library(wordcloud)
@@ -15,40 +22,162 @@ library(RColorBrewer)
 library(wordcloud2)
 library(tm)
 
-text_LA <- LA$text
-docs_LA <- Corpus(VectorSource(text_LA))
-docs_LA<- docs_LA %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removePunctuation) %>%
-  tm_map(stripWhitespace)
-docs_LA <- tm_map(docs_LA, content_transformer(tolower))
-docs_LA <- tm_map(docs_LA, removeWords, stopwords("english"))
-dtm <- TermDocumentMatrix(docs_LA) 
-matrix <- as.matrix(dtm) 
-words <- sort(rowSums(matrix),decreasing=TRUE) 
-df_LA <- data.frame(word = names(words),freq=words)
-png(filename = "LA.png")
-wordcloud(words = df_LA$word, freq = df_LA$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35,
-          colors=brewer.pal(8, "Dark2"))
+steps <- c("before", "during", "after")
+
+for (i in steps){
+  data_temp <- data %>%
+    filter(timeline == i)
+  text_data <- data_temp$text
+  docs_data <- Corpus(VectorSource(text_data))
+  docs_data<- docs_data %>%
+    tm_map(removeNumbers) %>%
+    tm_map(removePunctuation) %>%
+    tm_map(stripWhitespace)
+  docs_data <- tm_map(docs_data, content_transformer(tolower))
+  docs_data <- tm_map(docs_data, removeWords, stopwords("english"))
+  dtm <- TermDocumentMatrix(docs_data) 
+  matrix <- as.matrix(dtm) 
+  words <- sort(rowSums(matrix),decreasing=TRUE) 
+  df_data <- data.frame(word = names(words),freq=words) %>%
+    filter(word != "disaster" & word != "…" & word != "korea")
+
+  
+  png(filename = paste0("WC", i, ".png"), width = 1024, height = 1024, units = "px")
+  wordcloud(words = df_data$word, freq = df_data$freq, min.freq = 1,
+            max.words=200, random.order=FALSE, rot.per=0.35,
+            colors=brewer.pal(8, "Dark2"))
+  dev.off()
+  
+  assign(paste0("df", i), df_data)
+  
+}
+
+# get_sentiments("afinn")
+# get_sentiments("bing")
+# get_sentiments("nrc")
+
+#sentiment resilience
+ #BING
+png(filename = "bing_before.png",width = 1024, height = 1024, units = "px")
+dfbefore %>%
+  inner_join(get_sentiments("bing")) %>%
+  group_by(sentiment) %>%
+  slice_max(freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
 dev.off()
 
-
-text_ALL <- ALL$text
-docs_ALL <- Corpus(VectorSource(text_ALL))
-docs_ALL<- docs_ALL %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removePunctuation) %>%
-  tm_map(stripWhitespace)
-docs_ALL <- tm_map(docs_ALL, content_transformer(tolower))
-docs_ALL <- tm_map(docs_ALL, removeWords, stopwords("english"))
-dtm <- TermDocumentMatrix(docs_ALL) 
-matrix <- as.matrix(dtm) 
-words <- sort(rowSums(matrix),decreasing=TRUE) 
-df_ALL <- data.frame(word = names(words),freq=words)
-png(filename = "ALL.png")
-wordcloud(words = df_ALL$word, freq = df_ALL$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35,
-          colors=brewer.pal(8, "Dark2"))
+png(filename = "bing_during.png",width = 1024, height = 1024, units = "px")
+dfduring %>%
+  inner_join(get_sentiments("bing")) %>%
+  group_by(sentiment) %>%
+  slice_max(freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
 dev.off()
 
+png(filename = "bing_after.png",width = 1024, height = 1024, units = "px")
+dfafter %>%
+  inner_join(get_sentiments("bing")) %>%
+  group_by(sentiment) %>%
+  slice_max(freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+dev.off()
+
+ #NRC
+png(filename = "nrc_before.png", width = 1024, height = 1024, units = "px")
+dfbefore %>%
+  inner_join(get_sentiments("nrc")) %>%
+  mutate(temp = freq) %>%
+  mutate(temp2 = sentiment) %>%
+  pivot_wider(names_from = temp2, values_from = temp, values_fill = 0) %>% 
+  mutate(score = positive - negative) %>%
+  group_by(sentiment) %>%
+  slice_max(order_by = freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+dev.off()
+
+png(filename = "nrc_during.png", width = 1024, height = 1024, units = "px")
+dfduring %>%
+  inner_join(get_sentiments("nrc")) %>%
+  mutate(temp = freq) %>%
+  mutate(temp2 = sentiment) %>%
+  pivot_wider(names_from = temp2, values_from = temp, values_fill = 0) %>% 
+  mutate(score = positive - negative) %>%
+  group_by(sentiment) %>%
+  slice_max(order_by = freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+dev.off()
+
+png(filename = "nrc_after.png", width = 1024, height = 1024, units = "px")
+dfafter %>%
+  inner_join(get_sentiments("nrc")) %>%
+  mutate(temp = freq) %>%
+  mutate(temp2 = sentiment) %>%
+  pivot_wider(names_from = temp2, values_from = temp, values_fill = 0) %>% 
+  mutate(score = positive - negative) %>%
+  group_by(sentiment) %>%
+  slice_max(order_by = freq, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, freq)) %>%
+  ggplot(aes(freq, word, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~sentiment, scales = "free_y") +
+  labs(x = "Contribution to sentiment",
+       y = NULL)
+dev.off()
+
+ #afinn
+SCORE <- c()
+dfbefore <- dfbefore %>%
+  inner_join(get_sentiments("afinn")) %>%
+  mutate(score = freq*value)
+dfduring <- dfduring %>%
+  inner_join(get_sentiments("afinn")) %>%
+  mutate(score = freq*value)
+dfafter <- dfafter %>%
+  inner_join(get_sentiments("afinn")) %>%
+  mutate(score = freq*value)
+
+SCORE  <- c(sum(dfbefore$score), sum(dfduring$score), sum(dfafter$score))
+timeframe <- c("-1", "0", "1")
+names <- c("before", "during", "after")
+trend <- data.frame(names,timeframe, SCORE)
+
+png(filename = "social_resilience.png", width = 1024, height = 1024, units = "px")
+ggplot(trend, aes(x= factor(names,level = c("before", "during", "after")), SCORE, group = 1))+
+  geom_line(arrow = arrow())+
+  geom_point()+
+  xlab("timeline")+
+  ylab("emotion score")+
+  ggtitle("social resilience")+
+  theme(plot.title = element_text(hjust = 0.5))
+dev.off()
